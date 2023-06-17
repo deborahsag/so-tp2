3982bbefa272a55f472b4289409d6a2b99daba07
deborahsag <85948728+deborahsag@users.noreply.github.com>
2023-06-17 11:02:42 -0300

    Função swap, lista duplamente encadeada.

--- Documentacao.md
+++ Documentacao.md
@@ -1,9 +1,7 @@
 # Simulação de paginação
 
 ## Implementação da tambela de páginas
-Uma lista encadeada em que a própria célula guarda os dados. A lista possui uma cabeça dummy que guarda a informação do tamanho da lista. Essa informação foi encapsulada dessa forma para que o tamanho seja aumentado e diminuído nas próprias funções de adicionar e remover páginas.
-
-Uma vez que a paginação não é ordenada na memória, adotou-se a estratégia simplificada de sempre inserir novas páginas no final da lista encadeada.
+Uma lista duplamente encadeada em que a própria célula guarda os dados. A lista possui uma cabeça dummy que guarda a informação do tamanho da lista. Essa informação foi encapsulada dessa forma para que o tamanho seja aumentado e diminuído nas próprias funções de adicionar e remover páginas.
 
 Tendo em mente a simplicidade de implementação e considerando o tamanho reduzido da tabela de páginas, a busca é realizada sequencialmente pela lista.
 
--- tp2virtual.h
+++ tp2virtual.h
@@ -11,6 +11,7 @@ struct Page {
     int last_acc;
     int altered;
     int table_size;
+    Page *prev;
     Page *next;
 };
 
@@ -21,13 +22,14 @@ Page* init_page() {
     page = malloc (sizeof(Page));
     page->last_acc = 0;
     page->altered = 0;
+    page->prev = NULL;
     page->next = NULL;
     page->table_size = 0;
     return page;
 }
 
 
-void insert_in_table(unsigned addr, Page* page_table) {
+void insert_table_end(unsigned addr, Page* page_table) {
 /* Insere uma pagina na tabela de paginas baseado em seu endereco */
     Page *page = init_page();
     page->addr = addr;
@@ -38,27 +40,39 @@ void insert_in_table(unsigned addr, Page* page_table) {
     }
 
     last->next = page;
+    page->prev = last;
     page_table->table_size += 1;
 }
 
 
-void remove_from_table(Page* page_table, Page* prev, Page* page) {
-/* Remove uma celula da lista dada a cabeca da lista, a celula anterior e a celula a ser removida */    
-    prev->next = page->next;
-    page_table->table_size -= 1;
+void swap_page(Page* old, Page* new, Page* page_table) {
+/* Substitui uma pagina na tabela por outra dada a pagina anterior da que esta sendo substituida, a nova pagina e a tabela de pagina */
+    Page* prev = old->prev;
+    Page* next = old->next;
+    prev->next = new;
+    new->prev = prev;
+    next->prev = new;
+    new->next = next;
 }
 
 
-bool search_table(unsigned addr, Page* page_table) {
-/* Retorna true para um hit, retorna false para um miss */    
+// void remove_from_table(Page* page_table, Page* prev, Page* page) {
+// /* Remove uma celula da lista dada a cabeca da lista, a celula anterior e a celula a ser removida */    
+//     prev->next = page->next;
+//     page_table->table_size -= 1;
+// }
+
+
+Page* search_table(unsigned addr, Page* page_table) {
+/* Retorna a pagina anterior  */    
     Page *page = page_table;
     while(page->next != NULL){
         page = page->next;
         if (page->addr == addr){
-            return true;
+            return page;
         }
     }
-    return false;
+    return NULL;
 }
 
 
